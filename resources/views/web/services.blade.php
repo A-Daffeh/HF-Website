9cd410611d24db98ac1aa5c58f2492969acfd161
Sami Ali <samibaloch13@hotmail.com>
2023-04-05 15:08:45 -0700

    removed images from service pg and added paragrapgh to describe img

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -42,19 +42,30 @@
     </div>
 
     <div class="gallery">
-        <div>
-            <img src="{{ asset('img/gallery-1.jpeg') }}" alt="Gallery Img-1">
-            <p></p>
-            
+        <div class="gallery-div">
+            <img class ="gallery-img" src="{{ asset('img/gallery-1.jpeg') }}" alt="Gallery Img-1">
+            <p class="gallery-p">1</p>
         </div>
-        <img src="{{ asset('img/gallery-2.jpeg') }}" alt="Gallery Img-2">
+        <div class="gallery-div">
+            <img class ="gallery-img" src="{{ asset('img/gallery-2.jpeg') }}" alt="Gallery Img-2">
+            <p class="gallery-p">2</p>
+        </div>
+        <div class="gallery-div">
+            <img class ="gallery-img" src="{{ asset('img/gallery-6.jpeg') }}" alt="Gallery Img-6">
+            <p class="gallery-p">3</p>
+        </div>
+        <div class="gallery-div">
+            <img class ="gallery-img" src="{{ asset('img/gallery-8.jpeg') }}" alt="Gallery Img-8">
+            <p class="gallery-p">4</p>
+        </div>
+        <!-- <img src="{{ asset('img/gallery-2.jpeg') }}" alt="Gallery Img-2"> -->
         <!-- <img src="{{ asset('img/gallery-3.jpeg') }}" alt="Gallery Img-3"> -->
         <!-- <img src="{{ asset('img/gallery-4.jpeg') }}" alt="Gallery Img-4"> -->
-        <img src="{{ asset('img/gallery-5.jpeg') }}" alt="Gallery Img-5">
-        <img src="{{ asset('img/gallery-6.jpeg') }}" alt="Gallery Img-6">
-        <img src="{{ asset('img/gallery-7.jpeg') }}" alt="Gallery Img-7">
-        <img src="{{ asset('img/gallery-8.jpeg') }}" alt="Gallery Img-8">
-        <img src="{{ asset('img/gallery-9.jpeg') }}" alt="Gallery Img-9">
+        <!-- <img src="{{ asset('img/gallery-5.jpeg') }}" alt="Gallery Img-5"> -->
+        <!-- <img src="{{ asset('img/gallery-6.jpeg') }}" alt="Gallery Img-6"> -->
+        <!-- <img src="{{ asset('img/gallery-7.jpeg') }}" alt="Gallery Img-7"> -->
+        <!-- <img src="{{ asset('img/gallery-8.jpeg') }}" alt="Gallery Img-8"> -->
+        <!-- <img src="{{ asset('img/gallery-9.jpeg') }}" alt="Gallery Img-9"> -->
     </div>
     
 </section>
