0d034636eeae334edd55bcb7e98a63f3f4b4e52f
alieu22 <alieusma@gmail.com>
2023-04-27 21:30:27 -0700

    Made the feedback changes

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -1,6 +1,8 @@
 @extends('main')
 
 @section('content')
+
+
 <section class = "hf-sections">
     <div class = "hf-contactMain">
         <h1 class="h-first hf-he hf-contact-header hf-center">Interested in Contacting us? </h1>
@@ -10,27 +12,28 @@
             @csrf
 
             <div class = "hf-name">
-                <label class = "hf-labels" for="fullname">Name (John Doe)</label>
+                <label class = "hf-labels" for="fullname">Name * (John Doe)</label>
                 <br>
                 <input class = "hf-box" type="text" placeholder = "Enter your name" pattern="[a-zA-Z ]{1,30}" id = "fullname" name="full_name" title="(required)" required>
             </div>
 
             <div class = "hf-number">
-                <label class = "hf-Labels" for="phonenumber"> Phone Number (123-456-7890)</label>
+                <label class = "hf-Labels" for="phonenumber"> Phone Number * (123-456-7890)</label>
                 <br>
                 <input class = "hf-box" type="text" placeholder = "Enter your phone number"   pattern="^\d{3}-\d{3}-\d{4}$" id = "phonenumber" name="phone" title="(123-456-7890)" required>
             </div>
 
             <div class = "hf-email">
-                <label class = "hf-Labels" for="email">Email (abc@email.com) </label>
+                <label class = "hf-Labels" for="email">Email * (abc@email.com) </label>
                 
                 <input class = "hf-box" type="text" placeholder = "Enter your email"  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" id = "email" title="(required)" name="email" required >
             </div>
 
-            <div class="hf-comments"> 
-                 <label class="hf-Labels" for="comments">Reason for contact</label>
-                <textarea class="comment-box" rows="1500" cols="5" name="reason"></textarea>
-            </div>
+            <div class="hf-comments">
+             <label class="hf-Labels" for="comments">Reason for contact *</label>
+             <textarea class="comment-box" rows="5" name="reason" required></textarea>
+                </div>
+
             
 
              <div class = "hf-submit">
@@ -40,4 +43,7 @@
         </form>
     </div>
 </section>
+
+
+
 @endsection
