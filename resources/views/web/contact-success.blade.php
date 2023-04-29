c171a1c0916adc2dea80b9011fa2dad1a5c48267
alieu22 <alieusma@gmail.com>
2023-04-29 14:46:49 -0700

    Made the feedback changes

--- resources/views/web/contact-success.blade.php
+++ resources/views/web/contact-success.blade.php
@@ -4,12 +4,10 @@
 <section class = "hf-sections">
 
     <h1> Thank you for taking the time to contact us! We will get back to you as soon as possible <h1> 
-    <div class="cont-suc-img">
-        <img src="{{ asset('img/projectlogo.jpg') }}" alt="HF Logo" >
-    </div>
+    
     
     <div class="contact-suc-btn">
-        <a href="{{ route('hfhome') }}">Back to Home</a>
+        <a href="{{ route('hfhome') }}">OK</a>
     </div>
 </section>
 @endsection
